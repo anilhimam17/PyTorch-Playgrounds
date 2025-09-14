f9050f17c1d05e2aa4a6b2ebf4c2561220436bbe
anilhimam17 <anilhimamg@gmail.com>
2025-09-14 22:19:18 +0100

    feat: completed driver code for ViT

--- ViT/main.py
+++ ViT/main.py
@@ -1,18 +1,65 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
 import torch
 
-from src.multi_head_attention import MultiHeadSelfAttention
+from src.data import DataHandler
+from src.vision_transformer import VisionTransformer
+from src.optimizer import TrainingLoop
+
+
+# Accelerator Device
+DEVICE = torch.accelerator.current_accelerator()
+
+# Dataset Path
+dataset_path = "../reduced_imagenet"
+
+# Learning Curve Plot Path
+ASSET_PATH = Path("./assets")
 
 
 # The main function for the Vision Transformer
 def main():
-    print("Hello, Vision Transformer!!!")
+    # Initialising the DataHandler
+    data_handle = DataHandler(root_dir=dataset_path)
 
-    mhsa_block = MultiHeadSelfAttention()
-    X = torch.randn(64, 197, 768)
+    # Loading the Dataset
+    loaded_train_set = data_handle.load_set("train")
+    loaded_valid_set = data_handle.load_set("valid")
 
-    # Forward Prop
-    x = mhsa_block(X)
-    print("Attended Embeddings Shape: ", x.shape)
+    # Preparing the sets
+    train_set = data_handle.prepare_dataset(loaded_train_set)
+    valid_set = data_handle.prepare_dataset(loaded_valid_set)
+
+    # Initializing the Vision Transformer
+    first_vit = VisionTransformer(n_classes=len(data_handle.class_names)).to(device=DEVICE)
+    print("Model Architecture")
+    print(first_vit, end="\n\n")
+
+    num_params = sum(layer.numel() for layer in first_vit.parameters() if layer.requires_grad)
+    print(f"Total no of learnable params: {num_params}")
+
+    # Loading the Training Loop Handler
+    optimizer = TrainingLoop(learning_rate=1e-4, model=first_vit)
+
+    # Training and Validating the model
+    train_losses, valid_losses = optimizer.train_model(5, train_set=train_set, valid_set=valid_set)
+
+    # Plotting the losses
+    plt.figure(figsize=(10, 8))
+    plt.title("Learning Curve")
+    plt.plot(range(1, len(train_losses) + 1), train_losses, c="b", ls="-.", label="Train Loss")
+    plt.plot(range(1, len(valid_losses) + 1), valid_losses, c="g", ls="-*", label="Valid Loss")
+    plt.legend(loc="upper right")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+
+    # Storing the Learning Curve
+    if not ASSET_PATH.exists():
+        ASSET_PATH.mkdir()
+    plt.savefig(ASSET_PATH / "learning_curve.png")
+
+    # Rendering the plot
+    plt.show()
 
 
 # ==== Driver Code ====
