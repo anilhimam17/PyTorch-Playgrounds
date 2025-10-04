27f80db178ff85eb942a625021ed3bc2c8f9bf9a
anilhimam17 <anilhimamg@gmail.com>
2025-10-04 02:21:35 +0100

    feat: boiler plate for main with data handle

--- CLIP/main.py
+++ CLIP/main.py
@@ -1,8 +1,14 @@
-# The main function
+from src.data import DataHandler
+
+
+# The Main Function
 def main() -> None:
     print("Hello, CLIP")
+    data_handle = DataHandler()
 
 
 # Driver code
 if __name__ == "__main__":
+
+    # The Main Function
     main()
